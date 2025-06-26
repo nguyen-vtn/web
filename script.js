bc1950cee98faeb25443d234654ef954b4dfc9c3
nguyen-vtn <Nguyennguyen7b@gmail.com>
2025-06-26 23:35:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
   var password = document.getElementById("password").value;
-  if (password === "123456") {
+  if (password === "12345") {
     window.location.href = "home.html";
   } else {
     alert("Sai mật khẩu!");
